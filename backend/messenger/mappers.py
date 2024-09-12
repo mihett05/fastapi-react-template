8c1fcf00b7bcafe9c580f47c9c6516f7c9260294
Никита <nikitagyba@gmail.com>
2024-09-12 15:17:08 +0500

    подправил код 2.0

--- backend/messenger/mappers.py
+++ backend/messenger/mappers.py
@@ -4,10 +4,6 @@ from core.mappers import sqlalchemy_retort
 
 retort = sqlalchemy_retort.extend(recipe=[])
 
-messages_mapper = retort
-
 message_mapper = retort.get_converter(Message, MessageRead)
-
-chats_mapper = retort
 
 chat_mapper = retort.get_converter(Chat, ChatRead)
