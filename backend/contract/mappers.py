1474cdc1c70ae316b628f07d77984ba6f1d1b74d
Никита <nikitagyba@gmail.com>
2024-09-12 20:40:12 +0500

    подправление крудошлепства контракта (опять)

--- backend/contract/mappers.py
+++ backend/contract/mappers.py
@@ -1,9 +1,18 @@
+from adaptix.conversion import coercer
+
 from contract.models import Contract
 from contract.schemas import ContractRead
 from core.mappers import sqlalchemy_retort
+from messenger.mappers import chat_mapper
+from messenger.models import Chat
+from messenger.schemas import ChatRead
+from users.mappers import user_mapper
+from users.models import User
+from users.schemas import UserRead
 
 retort = sqlalchemy_retort.extend(recipe=[])
 
-contract_mapper = retort.get_converter(Contract, ContractRead)
+contract_mapper = retort.get_converter(Contract, ContractRead, recipe=[coercer(Chat, ChatRead, chat_mapper),
+                                                                       coercer(User, UserRead, user_mapper)])
 
 
